5b586d7f2d87e296e403d438d372aedce0d1b6e3
Lindsay Criswell <lindsaycriswell@gmail.com>
2017-10-08 21:43:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {Beatles: 'Hey Jude'};
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist.artistName = songTitle;
+  playlist[artistName] = songTitle;
   return playlist;
 }
 // function updatePlaylist(playlist, artistName, songTitle){
