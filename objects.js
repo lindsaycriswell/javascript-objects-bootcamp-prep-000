82e28453a0b7d09846be444b19a2f8d54b7a3e04
Lindsay Criswell <lindsaycriswell@gmail.com>
2017-10-08 21:40:26 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,13 +1,12 @@
-
-var playlist = new Object({ Beatles: 'Hey Jude'});
-
-
-function updatePlaylist(playlist, artistName, songTitle){
-  playlist = Object.assign(playlist, {[artistName]: songTitle});
-  return playlist;
-}
-
-function removeFromPlaylist(playlist, artistName){
-  delete playlist[artistName];
-  return playlist;
-}
+// var playlist = new Object({ Beatles: 'Hey Jude'});
+// 
+// 
+// function updatePlaylist(playlist, artistName, songTitle){
+//   playlist = Object.assign(playlist, {[artistName]: songTitle});
+//   return playlist;
+// }
+// 
+// function removeFromPlaylist(playlist, artistName){
+//   delete playlist[artistName];
+//   return playlist;
+// }
